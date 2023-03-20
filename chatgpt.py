5e0a48d4f9f654e6a604439fa8d8f6b98a108ec9
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-03-20 16:30:28 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -42,8 +42,8 @@ xa = bytearray.fromhex("68 74 74 70 73 3A 2F 2F 67 69 74 68 75 62 2E 63 6F 6D 2F
 axx = bytearray.fromhex("49  54 7A 5F 4C 45 47 45 4E 44 5F 43 4F 44 45 52").decode()
 xxc = bytearray.fromhex("4D 52 5F 53 55 4B 4B 55 4E").decode()
 SOURCE = xa
-DEVELOPER = axx
 UPDATE_CHNL = xxc
+DEVELOPER = axx
 SOURCE_TEXT = f"""
 ๏ ʜᴇʏ, ɪ ᴀᴍ [{BOT_NAME}]
 ➻ ᴀɴ ᴏᴘᴇɴ-ᴀɪ-ʙᴀsᴇᴅ ᴄʜᴀᴛɢᴘᴛ.
@@ -200,12 +200,13 @@ d= bytearray.fromhex("4D 52 5F 53 55 4B 4B 55 4E").decode()
 if SOURCE != s:
     print("So sad, you have changed source, change it back to ` https://github.com/Noob-mukesh/Chatgpt-bot`  else I won't work")
     sys.exit(1)  
-if DEVELOPER!=d:
-    print("So sad, you have change developer, change it back to itz_legend_coder else I won't work")
+if DEVELOPER!=u:
+    print("So sad, you have changed Updates, change it back to itz_legend_coder else I won't work")
     sys.exit(1)
-if UPDATE_CHNL!=u:
-    print("So sad, you have changed Updates, change it back to MR_SUKKUN else I won't work")
+if UPDATE_CHNL!=d:
+    print("So sad, you have change developer, change it back to MR_SUKKUN else I won't work")
     sys.exit(1)
+
 
 if name == "main":
     print(f""" {BOT_NAME} ɪs ᴀʟɪᴠᴇ!
