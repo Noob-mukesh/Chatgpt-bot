7e6ac842344ddca40208b52136a042f9175ab262
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-04-25 15:55:35 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -193,7 +193,45 @@ async def chat(bot, message):
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ:    {e} ")
 
+@Mukesh.on_message(filters.command(["image","photo","img","generate"],  prefixes=["","+", ".", "/", "-", "?", "$","#","&"] ))
 
+async def chat(bot, message):
+
+    try:
+
+
+
+        start_time = time.time()
+
+        await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
+
+        if len(message.command) < 2:
+
+            await message.reply_text(
+
+            "**Example:**\n\n`/generate a white siamese cat`")
+
+        else:
+
+            a = message.text.split(' ', 1)[1]
+
+            response= openai.Image.create(prompt=a ,n=1,size="1024x1024")
+
+            image_url = response['data'][0]['url']
+
+            end_time = time.time()
+
+            telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
+
+            await message.reply_photo(image_url,caption=f"✨ᴛɪᴍᴇ ᴛᴀᴋᴇɴ {telegram_ping} ",parse_mode=ParseMode.DISABLED,reply_markup=InlineKeyboardMarkup(X)) 
+
+    except Exception as e:
+
+            await message.reply_text(f"**ᴇʀʀᴏʀ: **  ` {e} `")
+
+    
+
+    
 s = bytearray.fromhex("68 74 74 70 73 3A 2F 2F 67 69 74 68 75 62 2E 63 6F 6D 2F 4E 6F 6F 62 2D 6D 75 6B 65 73 68 2F 43 68 61 74 67 70 74 2D 62 6F 74").decode()
 u = bytearray.fromhex("49  54 7A 5F 4C 45 47 45 4E 44 5F 43 4F 44 45 52").decode()
 d= bytearray.fromhex("4D 52 5F 53 55 4B 4B 55 4E").decode()
