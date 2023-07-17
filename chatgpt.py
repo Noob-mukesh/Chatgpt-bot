3746ff06d97842765f7994794f25d87d4bb4d6a2
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-07-17 23:09:30 +0530

    Removed encoding

--- chatgpt.py
+++ chatgpt.py
@@ -39,11 +39,7 @@ START = f"""
 ๏ ᴛᴏ ɢᴇᴛ ʜᴇʟᴘ ᴜsᴇ /help
 """
 xa = bytearray.fromhex("68 74 74 70 73 3A 2F 2F 67 69 74 68 75 62 2E 63 6F 6D 2F 4E 6F 6F 62 2D 6D 75 6B 65 73 68 2F 43 68 61 74 67 70 74 2D 62 6F 74").decode()
-axx = bytearray.fromhex("49  54 7A 5F 4C 45 47 45 4E 44 5F 43 4F 44 45 52").decode()
-xxc = bytearray.fromhex("4D 52 5F 53 55 4B 4B 55 4E").decode()
 SOURCE = xa
-UPDATE_CHNL = xxc
-DEVELOPER = axx
 SOURCE_TEXT = f"""
 ๏ ʜᴇʏ, ɪ ᴀᴍ [{BOT_NAME}]
 ➻ ᴀɴ ᴏᴘᴇɴ-ᴀɪ-ʙᴀsᴇᴅ ᴄʜᴀᴛɢᴘᴛ.
@@ -233,17 +229,10 @@ async def chat(bot, message):
 
     
 s = bytearray.fromhex("68 74 74 70 73 3A 2F 2F 67 69 74 68 75 62 2E 63 6F 6D 2F 4E 6F 6F 62 2D 6D 75 6B 65 73 68 2F 43 68 61 74 67 70 74 2D 62 6F 74").decode()
-u = bytearray.fromhex("49  54 7A 5F 4C 45 47 45 4E 44 5F 43 4F 44 45 52").decode()
-d= bytearray.fromhex("4D 52 5F 53 55 4B 4B 55 4E").decode()
+
 if SOURCE != s:
     print("So sad, you have changed source, change it back to ` https://github.com/Noob-mukesh/Chatgpt-bot `  else I won't work")
     sys.exit(1)  
-if DEVELOPER!=u:
-    print("So sad, you have changed Updates, change it back to `ITz_LEGEND_CODER ` else I won't work")
-    sys.exit(1)
-if UPDATE_CHNL!=d:
-    print("So sad, you have change developer, change it back to `MR_SUKKUN ` else I won't work")
-    sys.exit(1)
 
 
 if __name__ == "__main__":
