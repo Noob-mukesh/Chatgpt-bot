6db4f0c13a43a79e8d0edd75a4060bfebbc0f1c9
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2023-12-25 16:08:23 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -207,7 +207,7 @@ async def bard_bot(bot, message):
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ:  {e} ")
 
-    '''
+    
 openai.api_key = OPENAI_KEY
 @Mukesh.on_message(filters.command(["image","photo","img","generate"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"] ))
 async def chat(bot, message):
@@ -246,7 +246,7 @@ async def chat(bot, message):
         await message.reply_text(f"**ᴇʀʀᴏʀ: **  ` {e} `")
 
 
-
+'''
 s = bytearray.fromhex("68 74 74 70 73 3A 2F 2F 67 69 74 68 75 62 2E 63 6F 6D 2F 4E 6F 6F 62 2D 6D 75 6B 65 73 68 2F 43 68 61 74 67 70 74 2D 62 6F 74").decode()
 
 if SOURCE != s:
