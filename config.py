dfb9460bfadb90bc7db0c7923576b114b23452f2
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-02-11 11:38:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-API_ID = os.environ.get("API_ID", 28600524)
-API_HASH = os.environ.get("API_HASH", "64e338b41fbcf80d6ad1dc0ec2d4aa64")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6668643830:AAG84JdlDor2p7D1u2eT_Fp8hmiDUTxu_Rk")
+API_ID = os.environ.get("API_ID", "")
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 UPDATE_CHNL = os.environ.get("UPDATE_CHNL", "mr_sukkun")
 OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "DevanshXD")
 SUPPORT_GRP = os.environ.get("SUPPORT_GRP", "the_support_chat")
@@ -12,7 +12,6 @@ STKR = os.environ.get(
     "STKR",
     "CAACAgUAAx0CaYuwqgACyjVkV5IgG7ufiEHNHvpO38W5S-yKUwACKQgAAh7QuFb43XOj0Mt-yy8E",
 )
-# BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 DATABASE_URI = os.environ.get(
     "DATABASE_URI",
     "mongodb+srv://Mukesh01:mstboy@cluster0.8jwzl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
