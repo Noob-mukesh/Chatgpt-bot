808881067684fee7e01333352a41fcea95e458b1
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-04-11 11:59:47 +0530

    Update blackbox.py

--- Chatgpt/modules/blackbox.py
+++ Chatgpt/modules/blackbox.py
@@ -30,9 +30,7 @@ async def blackbox_chat(bot, message):
         x=response["results"]
         
         await message.reply_text(f"{x}\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{Mukesh.username} ",reply_markup=InlineKeyboardMarkup(gpt_button),quote=True,disable_web_page_preview =True)  
-        else:
-            pass
-
+        
             
     except requests.exceptions.RequestException as e:
         pass
